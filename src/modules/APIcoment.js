f1abd8e8705b7bba78117d5821cf2d067d8835d4
Chris <hakizachrist1@gmail.com>
2023-02-16 20:17:59 +0200

    Upadate files

--- src/modules/APIcoment.js
+++ src/modules/APIcoment.js
@@ -1,27 +1,28 @@
 export const saveData = async (id, username, comment) => {
-    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4MVrrJK7zcMBj3JVTSQH/comments';
-    
-    const comments = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-      body: JSON.stringify({
-        item_id: id,
-        username: username,
-        comment: comment,
-      }),
-    });
+  const url =
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4MVrrJK7zcMBj3JVTSQH/comments';
 
-    const res = comments.text();
-    return res;
+  const comments = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      item_id: id,
+      username: username,
+      comment: comment,
+    }),
+  });
 
-  }
-  
-  export const getData = async (id) => {
-    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4MVrrJK7zcMBj3JVTSQH/comments';
-    const indentifierurl = `${url}?item_id=${id}`
-    const response = await fetch(indentifierurl,);
-    const res = response.json();
-    return res
-  }
+  const res = comments.text();
+  return res;
+};
+
+export const getData = async (id) => {
+  const url =
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4MVrrJK7zcMBj3JVTSQH/comments';
+  const indentifierurl = `${url}?item_id=${id}`;
+  const response = await fetch(indentifierurl);
+  const res = response.json();
+  return res;
+};
--- src/modules/popupcoment.js
+++ src/modules/popupcoment.js
@@ -2,15 +2,13 @@ import { saveData, getData } from './APIcoment';
 
 const displaycomment = async (index) => {
   const comentData = await getData(index);
-  console.log('check data to display here:', comentData);
-  const displaycomm = document.getElementsByClassName('.displaycomment');
-  displaycomm.innerHTML = '';
-  for(let i = 0; i<2; i++) {
-    console.log(comentData[i].creation_date)
-    console.log(comentData[i].username)
-    console.log(comentData[i].comment)
-    displaycomm.innerHTML += `<li class="border">${comentData[i].creation_date}:${comentData[i].username}:${comentData[i].comment}</li>`;
-  };
+  console.log(comentData);
+  const displaycomm = document.querySelector('.displaycomment');
+  comentData.forEach((coment) => {
+    const eachComment = document.createElement('p');
+    displaycomm.appendChild(eachComment);
+    eachComment.innerHTML = `${coment.creation_date} : ${coment.username} : ${coment.comment}`;
+  });
 };
 
 const addcomment = (index) => {
@@ -19,62 +17,69 @@ const addcomment = (index) => {
   adding.addEventListener('click', async (e) => {
     const username = document.getElementById('name');
     const comment = document.getElementById('comment');
-    const savedData = saveData(item_id, username.value, comment.value);
-    console.log('check data saved:', savedData);
+    saveData(item_id, username.value, comment.value);
     username.value = '';
     comment.value = '';
     displaycomment(index);
-
   });
 };
 
 export const countcoment = async (index) => {
   const coment = await getData(index);
   let count = coment.length;
-  console.log("length is: ", count)
   return count;
 };
 
 const closePop = () => {
-  const popupclose = document.querySelector('.popup');
-  popupclose.style.display = 'none';
-}
+  const closeup = document.querySelector('.pop-up-close-btn');
+  closeup.addEventListener('click', () => {
+    const mealblur = document.querySelector('.meal');
+    let popupclose = document.querySelector('.popup');
+    popupclose.innerHTML = '';
+    popupclose.removeAttribute('style');
+    mealblur.style.filter = 'none';
+  });
+};
 
 export const displaycommentPopup = async (meal, likes, index) => {
   const addcoment = document.querySelectorAll('.comment');
   const mealblur = document.querySelector('.meal');
   addcoment.forEach((pop) => {
-    pop.addEventListener('click', async() => {
+    pop.addEventListener('click', async () => {
       if (Number(pop.id) === index) {
-        const totalcount = await countcoment(index)
+        const totalcount = await countcoment(index);
         const list = document.querySelector('.popup');
         const html = `
     <div class="item">
       <img class="popupimg" src="${meal.strMealThumb}" alt="food">
+      <p class="pop-up-close-btn">&#10005;</p>
     </div>
     <div class="popupname flexcolumn">
       <h1 class='popup_meal-name-card'>${meal.strMeal} <i class="fa-solid" id="heart" data-id = "${index}"></i></h1>
       <p class="popuptotal"> <span class='like'>${likes}</span> likes</p>
     </div>
-    <div class="addcoment flexcolumn"><h3 class="count">comment<span class="number">${totalcount}</span></h3></div>
-    <div class="displaycomment"> </div>
-    <form class="flexcolumn">
+    <div class="addcoment flexcolumn">
+    <h3 class="count">comment
+    <span class="number">${totalcount}</span>
+    </h3>
+    </div>
+    <div class="displaycomment flexcolumn"> </div>
+    <form class="flexcolumn form">
     <input type="text" id='name' placeholder='Your name' required>
     <input type="text" id='comment' placeholder='Your insights' required>
     <button type="submit" id="submit">Submit</button>
     </form>
     </div>`;
-        const image = document.createElement('img');
-        image.setAttribute('src', './Enabled.png');
-        image.setAttribute('class', 'close');
         const item = document.createElement('li');
         item.setAttribute('data-id', meal.id);
         item.classList.add('list');
         item.innerHTML = html;
-        item.appendChild(image);
+        const divitem = document.querySelector('.item');
         list.appendChild(item);
         list.style.display = 'block';
         mealblur.style.filter = 'blur(10px)';
+
+        closePop();
 
         addcomment(index);
         displaycomment(index);
@@ -82,6 +87,3 @@ export const displaycommentPopup = async (meal, likes, index) => {
     });
   });
 };
-
-
-
