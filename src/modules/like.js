8c2ccbfd842f2e1a64b9dd4c614af1ea6eef18f0
Chris <hakizachrist1@gmail.com>
2023-02-16 23:44:39 +0200

    Update files

--- src/modules/like.js
+++ src/modules/like.js
@@ -5,7 +5,7 @@ export const incrementLikes = async (index) => {
       method: 'post',
       body: JSON.stringify({ item_id: index }),
       headers: { 'content-type': 'application/json' },
-    },
+    }
   );
   const predata = res.text();
   return predata;
@@ -13,7 +13,7 @@ export const incrementLikes = async (index) => {
 
 export const likeItem = async () => {
   const res = await fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KHwd97Kg4JUFks1Mpn4d/likes',
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KHwd97Kg4JUFks1Mpn4d/likes'
   );
   const predata = res.json();
   return predata;
