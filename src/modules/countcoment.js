b4f94baf4cc525812b876871229700dddf530eec
Chris <hakizachrist1@gmail.com>
2023-02-17 10:55:37 +0200

    Update countcoment

--- src/modules/countcoment.js
+++ src/modules/countcoment.js
@@ -2,9 +2,7 @@ const countcoments = (arr) => {
      const comments = arr;
      let count = 0;
      comments.forEach((item) =>{ 
-       if(item.comment !== "") { 
          count += 1     
-       }
      }) 
      return count;
   };
